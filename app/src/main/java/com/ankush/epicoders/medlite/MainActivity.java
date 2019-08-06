6927d48021a45906274aff63a8f98fa66c3b21c3
ankush-01 <ankush9044045461@gmail.com>
2019-08-07 03:46:10 +0530

    .

--- app/src/main/java/com/ankush/epicoders/medlite/MainActivity.java
+++ app/src/main/java/com/ankush/epicoders/medlite/MainActivity.java
@@ -1,13 +1,44 @@
 package com.ankush.epicoders.medlite;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Menu menu;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu=menu;
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+        SearchView searchView=(SearchView) menu.findItem(R.id.search).getActionView();
+        SearchManager searchManager=(SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(getApplicationContext(),newText,Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar();
+
     }
 }
